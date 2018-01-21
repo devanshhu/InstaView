9aed7556a8630e80931994aa2eacef3c0c1dbf7b
devanshhu <devanshu9719@gmail.com>
2018-01-21 15:42:51 +0530

    initial commit

---
+++ image_downloader.py
@@ -0,0 +1,9 @@
+import urllib
+
+
+def image_downloader_(url, name):
+    try:
+        urllib.request.urlretrieve(url, name + '.jpg')
+        print('Download Successful.. Downloaded to ' + os.getcwd())
+    except:
+        print('sorry download failed ')
---
+++ main.py
@@ -0,0 +1,14 @@
+
+from soup_maker import make_soup
+from get_details import get_detail
+from gui1 import returnval,make_l
+
+rawurl=''
+if __name__=='__main__':
+	rawurl= input('Enter the Username:  ')
+	url= "https://www.instagram.com/"+str(rawurl)
+	#print(url)
+	soup= make_soup(url)
+	get_detail(soup)
+	# print('project Successful')
+	# print(soup.prettify())
