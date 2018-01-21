251a516494c9fac31243bce997e01c0db4ff4b46
devanshhu <devanshu9719@gmail.com>
2018-01-21 15:32:10 +0530

    Revert "Ready to Use CLI"
    This reverts commit 73a30876d8d6a72cd73d6530cad8f56f958092a6.

--- soup_maker.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-from requests import get
-
-def make_soup(url):
-	agent = {'User-Agent':'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'}	
-	htmltxt = get(url, headers=agent ).text
-	soup= BeautifulSoup(htmltxt,'lxml')
-	return soup
